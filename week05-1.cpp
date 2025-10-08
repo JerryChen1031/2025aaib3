be08c4db0ebd77993e2a3027f4bbacf6afd23173
JerryChen1031 <jerry_chen1031@icloud.com>
2025-10-08 11:48:06 +0800

    week05

---
+++ week05-1.cpp
@@ -0,0 +1,13 @@
+//week05-1.cpp
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        stringstream ss(s);
+        string word;
+        while(ss >> word ){
+
+        }
+        cout << word;
+        return word.length();
+    }
+};
